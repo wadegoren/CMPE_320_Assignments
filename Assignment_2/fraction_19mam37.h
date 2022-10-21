60c00cb49c8c2110c7c6abab63f5c4b3b6404324
mattmac02 <mattmac02@hotmail.ca>
2022-10-21 19:14:08 -0400

    More commenting

--- Assignment_2/fraction_19mam37.h
+++ Assignment_2/fraction_19mam37.h
@@ -10,9 +10,11 @@ using namespace std;
 
 class Fraction {
     public:
+        //Numerator and Denominator values
         int numeratorr;
         int denominatorr;
 
+        //Accessor methods
         const int& numerator() const;
         const int& denominator() const;
 
@@ -20,17 +22,18 @@ class Fraction {
 
         int GCD(const int& a, const int& b);    //Greatest common denominator function
 
-        //Unary Operators
+        //Unary Operator Overloading
         Fraction& operator++(); //Pre-increment
-        Fraction& operator-();  //Negation
         Fraction operator++(int);   //Post-increment
+        Fraction& operator-();  //Negation
 
+        //Binary Operator Overloading
         Fraction& operator+=(const Fraction& rightH);
-
         friend ostream& operator<<(ostream& out, const Fraction& fraction);
         friend istream& operator<<(istream& out, const Fraction fraction);
 };
 
+//Exception Class
 class FractionException : public exception {
 public:
     FractionException(const string& errorMessage);
@@ -39,8 +42,16 @@ private:
     string errorMessage;
 };
 
+//Binary Operator Overloading
 Fraction operator+(const Fraction& leftH, const Fraction& rightH);
 
+Fraction operator-(const Fraction& leftH, const Fraction& rightH);
+
+Fraction operator*(const Fraction& leftH, const Fraction& rightH);
+
+Fraction operator/(const Fraction& leftH, const Fraction& rightH);
+
+//Binary Comparison Operator Overloading
 bool operator<(const Fraction& leftH, const Fraction& rightH);
 
 bool operator>(const Fraction& leftH, const Fraction& rightH);
@@ -52,15 +63,5 @@ bool operator<=(const Fraction& leftH, const Fraction& rightH);
 bool operator==(const Fraction& leftH, const Fraction& rightH);
 
 bool operator!=(const Fraction& leftH, const Fraction& rightH);
-
-Fraction operator-(const Fraction& leftH, const Fraction& rightH);
-
-Fraction operator*(const Fraction& leftH, const Fraction& rightH);
-
-Fraction operator/(const Fraction& leftH, const Fraction& rightH);
-
-//ostream& operator<<(ostream& out, const Fraction& fraction);
-//
-//istream& operator<<(istream& out, const Fraction fraction);
 
 #endif //ASSIGNMENT_2_FRACTION_19MAM37_H
