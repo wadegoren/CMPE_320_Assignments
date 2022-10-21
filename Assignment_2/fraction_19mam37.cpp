ad6648cd2d8460ec7c6848ea3762ec9a68726930
Matthew MacEachern <19mam37@queensu.ca>
2022-10-21 18:58:21 -0400

    All fixed except +=

--- Assignment_2/fraction_19mam37.cpp
+++ Assignment_2/fraction_19mam37.cpp
@@ -6,68 +6,129 @@
 #include "fraction_19mam37.h"
 
 Fraction::Fraction(int numerator, int denominator) {
+    numeratorr = numerator != 0 || denominator != 1 ? numerator / GCD(numerator, denominator) : numerator;
+    denominatorr = numerator != 0 || denominator != 1 ? denominator / GCD(numerator, denominator) : denominator;
     if (denominator == 0) {
         throw FractionException("Denominator equals 0");
     }
-    if (numerator < 0 && denominator < 0){ //if both negative
-        numerator = numerator * -1;
-        denominator = denominator * -1;
-    }
-    if (numerator >= 0 && denominator < 0){
-        numerator = numerator * -1;
-        denominator = denominator * -1;
+    if (numeratorr < 0 && denominatorr < 0) { //if both negative
+        numeratorr = -numeratorr;
+        denominatorr = -denominatorr;
+    } else if (numeratorr >= 0 && denominatorr < 0){
+        numeratorr = -numeratorr;
+        denominatorr = -denominatorr;
     }
 }
 
+int Fraction::GCD(const int& a, const int& b) {
+    if (b == 0 || a == 0) return 1;
+    int a_abs = abs(a);
+    int b_abs = abs(b);
+    if (b_abs <= a_abs && a_abs % b_abs == 0) {
+        return b_abs;
+    } else if (a_abs < b_abs) {
+        return GCD(b_abs, a_abs);
+    } else {
+        return GCD(b_abs, a_abs % b_abs);
+    }
+}
 
-ostream& operator<<(ostream& out, const Fraction fraction){
+ostream& operator<<(ostream& out, const Fraction& fraction){
     out << fraction.numeratorr  << '/' << fraction.denominatorr;
     return out;
 }
 
-istream& operator<<(istream& out, const Fraction fraction){
+istream& operator<<(istream& out, const Fraction& fraction){
     out << fraction.numeratorr  << '/' << fraction.denominatorr;
     return out;
 }
 
 Fraction operator+(const Fraction& leftH, const Fraction& rightH){
-    int numer = (leftH.getNumerator() * rightH.getDenominator()) + (rightH.getNumerator() * leftH.getDenominator());
-    int denom = leftH.getDenominator() * rightH.getDenominator();
+    int numer = (leftH.numerator() * rightH.denominator()) + (rightH.numerator() * leftH.denominator());
+    int denom = leftH.denominator() * rightH.denominator();
     return Fraction(numer, denom);
 }
 
 Fraction operator-(const Fraction& leftH, const Fraction& rightH){
-    int numer = (leftH.getNumerator() * rightH.getDenominator()) - (rightH.getNumerator() * leftH.getDenominator());
-    int denom = leftH.getDenominator() * rightH.getNumerator();
+    int numer = (leftH.numerator() * rightH.denominator()) - (rightH.numerator() * leftH.denominator());
+    int denom = leftH.denominator() * rightH.numerator();
     return Fraction(numer, denom);
 }
 
 Fraction operator*(const Fraction& leftH, const Fraction& rightH){
-    int numer = leftH.getNumerator() * rightH.getNumerator();
-    int denom = leftH.getDenominator() * rightH.getDenominator();
+    int numer = leftH.numerator() * rightH.numerator();
+    int denom = leftH.denominator() * rightH.denominator();
     return Fraction(numer, denom);
 }
 
 Fraction operator/(const Fraction& leftH, const Fraction& rightH){
-    int numer = leftH.getNumerator() * rightH.getDenominator();
-    int denom = leftH.getDenominator() * rightH.getNumerator();
+    int numer = leftH.numerator() * rightH.denominator();
+    int denom = leftH.denominator() * rightH.numerator();
     return Fraction(numer, denom);
 }
 
 Fraction& Fraction::operator++(){   //pre-increment
-    numeratorr = getDenominator() + getNumerator();
+    numeratorr = denominator() + numerator();
     return *this;
 }
 
 Fraction Fraction::operator++(int unused) { //post-increment
     Fraction clone(numeratorr, denominatorr);
-    numeratorr = getDenominator() + getNumerator();
+    numeratorr = denominator() + numerator();
     return clone;
 }
 
+bool operator<(const Fraction& leftH, const Fraction& rightH){
+    if (leftH.numerator() * rightH.denominator() < rightH.numerator() * leftH.denominator()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool operator>(const Fraction& leftH, const Fraction& rightH){
+    if (leftH.numerator() * rightH.denominator() > rightH.numerator() * leftH.denominator()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool operator>=(const Fraction& leftH, const Fraction& rightH){
+    if (leftH.numerator() * rightH.denominator() >= rightH.numerator() * leftH.denominator()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool operator<=(const Fraction& leftH, const Fraction& rightH){
+    if (leftH.numerator() * rightH.denominator() <= rightH.numerator() * leftH.denominator()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool operator==(const Fraction& leftH, const Fraction& rightH){
+    if (leftH.numerator() * rightH.denominator() == rightH.numerator() * leftH.denominator()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool operator!=(const Fraction& leftH, const Fraction& rightH){
+    if (leftH.numerator() * rightH.denominator() != rightH.numerator() * leftH.denominator()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
 Fraction& Fraction::operator+=(const Fraction& rightH){
-    numeratorr = numeratorr + rightH.numeratorr;
-    denominatorr = denominatorr + rightH.denominatorr;
+    numeratorr = (numeratorr * rightH.numeratorr) + (rightH.numeratorr * denominatorr);
+    denominatorr = denominatorr * rightH.denominatorr;
     return *this;
 }
 
@@ -76,11 +137,11 @@ Fraction& Fraction::operator-(){
     return *this;
 }
 
-const int& Fraction::getNumerator() const{
+const int& Fraction::numerator() const{
     return numeratorr;
 }
 
-const int& Fraction::getDenominator() const{
+const int& Fraction::denominator() const{
     return denominatorr;
 }
 
--- Assignment_2/fraction_19mam37.h
+++ Assignment_2/fraction_19mam37.h
@@ -10,19 +10,42 @@ using namespace std;
 
 class Fraction {
     public:
-        const int& getNumerator() const;
-        const int& getDenominator() const;
+        const int& numerator() const;
+        const int& denominator() const;
         Fraction(int numerators = 0, int denominators = 1);
-//        Fraction(int numFrac);
+        int GCD(const int& a, const int& b);
         Fraction& operator++();
         Fraction& operator-();
         Fraction operator++(int);
-//    private:
+        Fraction& operator+=(const Fraction& rightH);
         int numeratorr;
         int denominatorr;
+        friend ostream& operator<<(ostream& out, const Fraction& fraction);
+
+        friend istream& operator<<(istream& out, const Fraction fraction);
+};
+
+class FractionException : public exception {
+public:
+    FractionException(const string& errorMessage);
+    string& what();
+private:
+    string errorMessage;
 };
 
 Fraction operator+(const Fraction& leftH, const Fraction& rightH);
+
+bool operator<(const Fraction& leftH, const Fraction& rightH);
+
+bool operator>(const Fraction& leftH, const Fraction& rightH);
+
+bool operator>=(const Fraction& leftH, const Fraction& rightH);
+
+bool operator<=(const Fraction& leftH, const Fraction& rightH);
+
+bool operator==(const Fraction& leftH, const Fraction& rightH);
+
+bool operator!=(const Fraction& leftH, const Fraction& rightH);
 
 Fraction operator-(const Fraction& leftH, const Fraction& rightH);
 
@@ -30,16 +53,8 @@ Fraction operator*(const Fraction& leftH, const Fraction& rightH);
 
 Fraction operator/(const Fraction& leftH, const Fraction& rightH);
 
-ostream& operator<<(ostream& out, const Fraction& fraction);
-
-istream& operator<<(istream& out, const Fraction fraction);
-
-class FractionException : public exception {
-    public:
-        FractionException(const string& errorMessage);
-        string& what();
-    private:
-        string errorMessage;
-};
+//ostream& operator<<(ostream& out, const Fraction& fraction);
+//
+//istream& operator<<(istream& out, const Fraction fraction);
 
 #endif //ASSIGNMENT_2_FRACTION_19MAM37_H
